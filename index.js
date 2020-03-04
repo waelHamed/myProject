07cde0f3dfb5c71f4a96564f727ec7b75b2c44d1
itachi <itachi@localhost.localdomain>
2020-03-05 00:21:51 +0200

    Second commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+/*jshint esversion: 6 */
+
 const express = require('express')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('This my test travis project'))
+app.get('/', (req, res) => res.send('This my test travis project'));
 
 app.listen(port, () => console.log(`App port ${port}!`))
