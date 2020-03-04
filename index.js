02f6f68d00c23e15f25a136a34387380cdf198a3
itachi <itachi@localhost.localdomain>
2020-03-05 00:36:25 +0200

    fixed errors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /*jshint esversion: 6 */
 
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('This my test travis project'));
 
-app.listen(port, () => console.log(`App port ${port}!`))
+app.listen(port, () => console.log(`App port ${port}!`));
